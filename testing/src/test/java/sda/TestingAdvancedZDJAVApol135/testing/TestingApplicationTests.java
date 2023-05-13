6285cc2e7ab5293e73164eaa96d5ad281b42ee8b
Maciej Burzyński <maciej.kollaps@gmail.com>
2023-05-13 10:59:51 +0200

    Init springboot project for advanced testing classes

---
+++ testing/src/test/java/sda/TestingAdvancedZDJAVApol135/testing/TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package sda.TestingAdvancedZDJAVApol135.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
